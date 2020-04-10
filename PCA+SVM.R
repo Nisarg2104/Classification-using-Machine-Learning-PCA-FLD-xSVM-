a4442158385fbdf48156c5da8fa47c32be97ab69
Nisarg2104 <nisargvora2104@gmail.com>
2020-04-10 12:00:07 +0530

    Eliminated Libraries for PCA and FLD

--- PCA+SVM.R
+++ PCA+SVM.R
@@ -6,46 +6,91 @@ str(winedataset)
 #Splitting Data into Training and Testing Dataset
 library(caTools)
 set.seed(1)
-s <-sample.split(winedataset$Category, SplitRatio = 0.8)
+s <-sample.split(winedataset$Category, SplitRatio = 0.82)
 t1<-cbind(winedataset,s=s)
 trainData<-subset(t1,s==TRUE)[1:14]
+
+mean(trainData1$Proline)
+
+trainData[2:14]= as.data.frame(scale(trainData[2:14],center=TRUE, scale = TRUE))
+#for(i in c(2:14)){
+ # m1<-mean(trainData[,i])
+  #for(j in c(1:nrow(trainData)))
+ # {
+#    k<-(trainData[j,i]-m1)
+ #   trainData[j,i]<-k
+#  }
+#}
+
+m<-cov(trainData[,2:14])
+
+ev<-eigen(m)
+values<-ev$values
+vectors<-ev$vectors
+
+l<-matrix(1:(nrow(trainData)*2),nrow=nrow(trainData))
+
+for(i in c(1:nrow(trainData))){
+  sum<-0
+  for(j in c(2:14)){
+    sum<-(sum+vectors[j-1,2]*trainData[i,j])
+  }
+  l[i,2] <- sum
+}
+for(i in c(1:nrow(trainData))){
+  sum<-0
+  for(j in c(2:14)){
+    sum<-(sum+vectors[j-1,1]*trainData[i,j])
+  }
+  l[i,1] <- sum
+}
+
+
+plot(l)
 testData<-subset(t1,s==FALSE)[1:14]
+testData[2:14]= as.data.frame(scale(testData[2:14],center=TRUE, scale = TRUE))
 
-#Applying PCA
-d01<-trainData[,2:14]
-d01.pca<-princomp(d01,cor=TRUE, score=TRUE)
-summary(d01.pca)
-d01.pca
+lt<-matrix(1:(nrow(testData)*2),nrow=nrow(testData))
 
-plot(d01.pca)
-d01.pca$loadings
-d01.pca$scores
-plot(d01.pca$scores[,1:2])
+for(i in c(1:nrow(testData))){
+  sum<-0
+  for(j in c(2:14)){
+    sum<-(sum+vectors[j-1,2]*testData[i,j])
+  }
+  lt[i,2] <- sum
+}
+for(i in c(1:nrow(testData))){
+  sum<-0
+  for(j in c(2:14)){
+    sum<-(sum+vectors[j-1,1]*testData[i,j])
+  }
+  lt[i,1] <- sum
+}
 
-d02<-testData[,2:14]
-d02.pca<-princomp(d02,cor=TRUE, score=TRUE)
-summary(d02.pca)
 
-plot(d02.pca)
-d02.pca$loadings
-d02.pca$scores
-plot(d02.pca$scores[,1:2])
+
 #Using SVM
 library(e1071)
 
 library(ggplot2)
-dat<-cbind.data.frame(y=trainData[,1],d01.pca$scores[,1:2])
-dat1<-cbind.data.frame(y=testData[,1],d02.pca$scores[,1:2])
+d<-data.frame(Comp.1=l[,1],Comp.2=l[,2])
+dt<-data.frame(Comp.1=lt[,1],Comp.2=lt[,2])
+e<-as.factor(trainData[,1])
+dat<-cbind.data.frame(y=e,d)
+dat1<-cbind.data.frame(y=testData[,1],dt)
+
 
 
 qplot(Comp.1, Comp.2, data =dat , color = factor(y)) + geom_point(shape = 1) +scale_colour_manual(values = c("#0000FF", "#00FF00","#FF0000"), labels = c("1", "2","3"))
 svm1 <- svm(y ~ .,data=dat, type="C-classification", kernel="radial", gamma=0.1, cost=10)
 svm1$SV
 plot(svm1,dat,Comp.2~Comp.1)
+
 prediction<-predict(svm1,dat1)
 xtab<-table(dat1$y,prediction)
 xtab
 sum(diag(xtab))/nrow(dat1)
 class(xtab)
 class(svm1)
+
 
--- mlcodeFLD.R
+++ mlcodeFLD.R
@@ -9,18 +9,119 @@ library(caTools)
 s <-sample.split(winedataset$Category, SplitRatio = 0.82)
 t1<-cbind(winedataset,s=s)
 trainData<-subset(t1,s==TRUE)[1:14]
+trainData[2:14]= as.data.frame(scale(trainData[2:14],center=TRUE, scale = TRUE))
+
+
 testData<-subset(t1,s==FALSE)[1:14]
-library(MASS)
-trainData.lda<-lda(Category ~.,trainData)
-trainData.lda
-trainData.ldascores<-predict(trainData.lda)
+testData[2:14]= as.data.frame(scale(testData[2:14],center=TRUE, scale = TRUE))
 
-plot(trainData.lda)
-dat<-cbind.data.frame(y=trainData[,1],LD1=trainData.ldascores$x[,1],LD2=trainData.ldascores$x[,2])
 
-testData.ldascores <- predict(trainData.lda,testData)
-testData.ldascores
-data1<-cbind.data.frame(y=testData[,1],LD1=testData.ldascores$x[,1],LD2=testData.ldascores$x[,2])
+class1<-as.matrix(subset(trainData,Category==1)[2:14])
+class2<-as.matrix(subset(trainData,Category==2)[2:14])
+class3<-as.matrix(subset(trainData,Category==3)[2:14])
+class<-as.matrix(trainData[2:14])
+m1<-c(0,0,0,0,0,0,0,0,0,0,0,0,0)
+for(i in c(1:nrow(class1))){
+  m1=m1+class1[i,]
+}
+m1<-m1/nrow(class1)
+m2<-c(0,0,0,0,0,0,0,0,0,0,0,0,0)
+for(i in c(1:nrow(class2))){
+  m2=m2+class2[i,]
+}
+m2<-m2/nrow(class2)
+m3<-c(0,0,0,0,0,0,0,0,0,0,0,0,0)
+for(i in c(1:nrow(class3))){
+  m3=m3+class3[i,]
+}
+m3<-m3/nrow(class3)
+
+m<-c(0,0,0,0,0,0,0,0,0,0,0,0,0)
+for(i in c(1:nrow(class))){
+  m=m+class[i,]
+}
+m<-m/nrow(class)
+
+
+for(i in c(1:nrow(class1))){
+  class1[i,]=class1[i,]-m1
+}
+
+for(i in c(1:nrow(class2))){
+  class2[i,]=class2[i,]-m2
+}
+
+for(i in c(1:nrow(class3))){
+  class3[i,]=class3[i,]-m3
+}
+
+
+sw1=0
+for(i in c(1:nrow(class1))){
+  sw1=sw1+((class1[i,]-m1)%*%t((class1[i,]-m1)))
+}
+
+sw2=0
+for(i in c(1:nrow(class2))){
+  sw2=sw2+((class2[i,]-m2)%*%t((class2[i,]-m2)))
+}
+
+sw3=0
+for(i in c(1:nrow(class3))){
+  sw3=sw3+((class3[i,]-m3)%*%t((class3[i,]-m3)))
+}
+
+sw=sw1+sw2+sw3
+
+sb1=(nrow(class1)*((m1-m)%*%t((m1-m))))
+sb2=(nrow(class2)*((m2-m)%*%t((m2-m))))
+sb3=(nrow(class3)*((m3-m)%*%t((m3-m))))
+
+sb=(sb1+sb2+sb3)
+library(matlib)
+swI<-inv(sw)
+a<-swI%*%sb
+b<-eigen(a)
+vectors<-b$vectors
+l<-matrix(1:(nrow(trainData)*2),nrow=nrow(trainData))
+
+for(i in c(1:nrow(trainData))){
+  sum<-0
+  for(j in c(2:14)){
+    sum<-(sum+vectors[j-1,2]*trainData[i,j])
+  }
+  l[i,2] <- sum
+}
+for(i in c(1:nrow(trainData))){
+  sum<-0
+  for(j in c(2:14)){
+    sum<-(sum+vectors[j-1,1]*trainData[i,j])
+  }
+  l[i,1] <- sum
+}
+plot(l[,2]~l[,1])
+lt<-matrix(1:(nrow(testData)*2),nrow=nrow(testData))
+
+for(i in c(1:nrow(testData))){
+  sum<-0
+  for(j in c(2:14)){
+    sum<-(sum+vectors[j-1,2]*testData[i,j])
+  }
+  lt[i,2] <- sum
+}
+for(i in c(1:nrow(testData))){
+  sum<-0
+  for(j in c(2:14)){
+    sum<-(sum+vectors[j-1,1]*testData[i,j])
+  }
+  lt[i,1] <- sum
+}
+
+
+dat<-cbind.data.frame(y=trainData[,1],data.frame(LD1=as.numeric(l[,1]),LD2=as.numeric(l[,2])))
+
+
+data1<-cbind.data.frame(y=testData[,1],data.frame(LD1=as.numeric(lt[,1]),LD2=as.numeric(lt[,2])))
 
 library(e1071)
 svm1 <- svm(y ~ .,data=dat, type="C-classification", kernel="radial", gamma=0.1, cost=10)
